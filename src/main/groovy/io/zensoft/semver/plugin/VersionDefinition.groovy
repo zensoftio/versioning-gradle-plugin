1495be47beef0b89cde2b74ebf4e4ae5ad01f234
gbisiarin <george.bisiarin@zensoft.io>
2018-08-27 12:36:36 +0300

    init plugin

---
+++ src/main/groovy/io/zensoft/semver/plugin/VersionDefinition.groovy
@@ -0,0 +1,54 @@
+package io.zensoft.semver.plugin
+
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.Ref
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.revwalk.RevWalk
+
+class VersionDefinition {
+
+    private Integer majorVersion = 0
+    private Integer minorVersion = 0
+    private Integer patchVersion = 0
+    private Boolean snapshot = true
+
+    private static final String TAG_PATTERN = "[vV]\\d{1,2}.\\d{1,2}.\\d{1,2}"
+
+    VersionDefinition(Repository repo) {
+        Git git = new Git(repo)
+        List<Ref> tags = git.tagList().call()
+        if (!tags.isEmpty()) {
+            Ref lastTag = tags.last()
+            String refName = lastTag.name.substring(lastTag.name.lastIndexOf('/') + 1)
+            if (!refName.matches(TAG_PATTERN)) {
+                throw new IllegalArgumentException("Wrong version format in tag name")
+            }
+            String[] targetVersion = refName[1..-1].split("\\.")
+            this.majorVersion = new Integer(targetVersion[0])
+            this.minorVersion = new Integer(targetVersion[1])
+            this.patchVersion = new Integer(targetVersion[2])
+            String headCommit = git.log().setMaxCount(1).call().last()
+            String tagCommit = new RevWalk(repo).parseCommit(lastTag.objectId).toObjectId().name
+            this.snapshot = tagCommit != headCommit
+        }
+        if (this.snapshot) {
+            minorVersion++
+        }
+    }
+
+    Integer getMajorVersion() {
+        return majorVersion
+    }
+
+    Integer getMinorVersion() {
+        return minorVersion
+    }
+
+    Integer getPatchVersion() {
+        return patchVersion
+    }
+
+    String getVersion() {
+        return "$majorVersion.$minorVersion.$patchVersion${snapshot ? "-SNAPSHOT" : ""}"
+    }
+}
---
+++ src/main/groovy/io/zensoft/semver/plugin/VersioningPlugin.groovy
@@ -0,0 +1,21 @@
+package io.zensoft.semver.plugin
+
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class VersioningPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder()
+        Repository repo = repositoryBuilder
+                .setGitDir(new File("${project.projectDir}/.git"))
+                .setMustExist(true)
+                .build()
+        VersionDefinition versionDefinition = new VersionDefinition(repo)
+        project.version = versionDefinition.getVersion()
+    }
+
+}
